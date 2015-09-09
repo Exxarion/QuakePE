121f54ee3a5d7beed02c1847cd4afaf4b14992e9
Blumieare Exxarion <Apluscomics@live.com>
2015-09-09 19:54:25 -0400

    Create Main.php

---
+++ src/QuakePE/Main.php
@@ -0,0 +1,11 @@
+<?php
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase
+{
+//TODO: Everything
+}
